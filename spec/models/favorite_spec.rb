0ce2afd3a5b4c580e0650fdcc3ab4e091bcac6ef
Aldana Abril Longhi <aldulonghi@gmail.com>
2020-10-22 18:48:32 -0300

    Improve spec tests

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -4,14 +4,18 @@ RSpec.describe Favorite, type: :model do
 
     before :each do
         @user = User.create(id: 1, name: 'aldana', email: 'aldu@gmail.com', password: '123456', password_confirmation: '123456')
-        @user1 = User.create(name: 'aldu1234', email: 'aldu123@gmail.com')
-        @user2 = User.create(id: 2, name: 'aldulonghi', email: 'aldulonghi@gmail.com', password: '123456', password_confirmation: '123456')
-        @opinion = @user.opinions.build(content: 'hello, this a opinion for testing')
-        @favorite = @user.favorites.build(opinion_id: @opinion.id)
-        @followed = Following.create(follower_id: 1, followed_id: @user2.id)
-      end
+        @opinion = Opinion.create(author_id: 1, content: 'hello, this a opinion for testing')
+        @favorite = Favorite.create(opinion_id: @opinion.id, user_id: @user.id)
+    end
 
   it { should belong_to(:user) }
   it { should belong_to(:opinion) }
+  
+  it 'returns the user who created the object' do
+    expect(@favorite.user).to eql(@user)
+  end
 
+  it 'returns the opinion who recieved the favorite' do
+    expect(@favorite.opinion).to eql(@opinion)
+  end
 end
--- spec/models/following_spec.rb
+++ spec/models/following_spec.rb
@@ -1,8 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Following, type: :model do
+    before :each do
+        @user = User.create(id: 1, name: 'aldana', email: 'aldu@gmail.com', password: '123456', password_confirmation: '123456')
+        @user2 = User.create(id: 2, name: 'aldulonghi', email: 'aldulonghi@gmail.com', password: '123456', password_confirmation: '123456')
+        @opinion = Opinion.create(author_id: 1, content: 'hello, this a opinion for testing')
+        @following = Following.create(followed_id: @user.id, follower_id: @user2.id)
+    end
 
   it { should belong_to(:follower) }
   it { should belong_to(:followed) }
+  
+  it 'returns the user who created the object' do
+    expect(@favorite.user).to eql(@user)
+  end
 
+  it 'returns the opinion who recieved the favorite' do
+    expect(@favorite.opinion).to eql(@opinion)
+  end
 end
